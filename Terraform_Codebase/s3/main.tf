79e88896e972e4c4c837a260236625bdeca9cae2
ankityadav16 <38921315+ankityadav16@users.noreply.github.com>
2022-03-04 19:52:21 +0530

    Update main.tf

--- Terraform_Codebase/s3/main.tf
+++ Terraform_Codebase/s3/main.tf
@@ -2,16 +2,16 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_s3_bucket" "chiranjeeviam-s3" {
+resource "aws_s3_bucket" "ankityadav-s3" {
   bucket_prefix = var.bucket_prefix
   tags = var.tags
 }
 
-resource "aws_s3_bucket_acl" "chiranjeeviam" {
+resource "aws_s3_bucket_acl" "ankityadav" {
   bucket = aws_s3_bucket.chiranjeeviam-s3.id
   acl    = var.acl
 }
-resource "aws_s3_bucket_versioning" "versioning_chiranjeeviam" {
+resource "aws_s3_bucket_versioning" "versioning_ankityadav" {
   bucket = aws_s3_bucket.chiranjeeviam-s3.id
   versioning_configuration {
     status = "Enabled"
